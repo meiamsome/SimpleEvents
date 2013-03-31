194b32fff48928c429432ddeb5ae26b8c75e29a0
Oliver Wright <meiamsome@hotmail.com>
2013-04-01 00:48:14 +0100

    Load Games

--- src/me/meiamsome/simpleevents/GameTypesHandler.java
+++ src/me/meiamsome/simpleevents/GameTypesHandler.java
@@ -1,24 +1,37 @@
 package me.meiamsome.simpleevents;
 
-import java.util.List;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class GameTypesHandler {
-	static List<GameType> gameTypes = new ArrayList<GameType>();
+	static Map<String, GameType> gameTypes = new HashMap<String, GameType>();
 	static List<Game> games = new ArrayList<Game>();
+	static File base;
 	static boolean isLoaded = false;
 	
 	//add a game type.
-	public static void addType(GameType type) {
-		gameTypes.add(type);
+	public static void addType(String name, GameType type) {
+		gameTypes.put(name, type);
 		if(isLoaded) {
-			load(type);
+			load(name);
 		}
 	}
 	
-	public static void load(GameType type) {
-		//TODO: load games
-		//type.loadGames(baseFolder)
+	public static void load(File baseFolder) {
+		if(isLoaded) return;
+		base = baseFolder;
+		for(String name: gameTypes.keySet()) {
+			load(name);
+		}
+	}
+	
+	public static void load(String name) {
+		List<Game> ret = gameTypes.get(name).loadGames(new File(base, name));
+		if(ret == null || ret.size() == 0) return;
+		games.addAll(ret);
 	}
 	
 	public static List<Game> getAllGames() {
