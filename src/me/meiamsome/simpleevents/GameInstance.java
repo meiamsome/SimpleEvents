69513bc2ba368ccab6a5bf2477f75f4911bd97ef
Oliver Wright <meiamsome@hotmail.com>
2013-03-31 22:09:58 +0100

    Moved some things around

--- src/me/meiamsome/simpleevents/GameInstance.java
+++ src/me/meiamsome/simpleevents/GameInstance.java
@@ -4,35 +4,20 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.script.Invocable;
-import javax.script.ScriptException;
+public abstract class GameInstance {
+	Game parent;
 
-import me.meiamsome.simpleevents.events.BailEvent;
-
-import org.bukkit.event.Event;
-
-
-public class GameInstance {
-	Game game;
-	Object actualGame;
-	GameInstance(Game par, Object o) {
-		game = par;
-		actualGame = o;
+	public GameInstance(Game par) {
+		parent = par;
 	}
-	
-	public void executeEvent(Event e) throws ScriptException {
-		try {
-			((Invocable)game.engine).invokeMethod(actualGame, e.getEventName(), e);
-		} catch(NoSuchMethodException e1) {}//Do nothing
-	}
-	
+	//public abstract void bail(String[] log);
 	public void bail(String[] log) {
-		File f = new File(game.baseFolder, "errors.log");
+		File f = new File(parent.baseFolder, "errors.log");
 		FileWriter out;
 		try {
 			out = new FileWriter(f);
 		} catch (IOException e1) {
-			game.parent.log.severe("Could not log error in game '"+game.name+"'");
+			SimpleEvents.log.severe("Could not log error in game '"+parent.name+"'");
 			return;
 		}
 		try {
@@ -40,17 +25,7 @@ public class GameInstance {
 			for(String l: log) str += l + "\r\n";
 			out.append(str);
 		} catch(IOException e) {
-			game.parent.log.severe("Could not log error in game '"+game.name+"'");
-		}
-		try {
-			executeEvent(new BailEvent(game));
-		} catch (ScriptException e) {
-			try {
-				out.append("Error while bailing!");
-			} catch (IOException e2) {
-				
-			}
-			
+			SimpleEvents.log.severe("Could not log error in game '"+parent.name+"'");
 		}
 		try {
 			out.close();
--- src/me/meiamsome/simpleevents/SimpleEvents.java
+++ src/me/meiamsome/simpleevents/SimpleEvents.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SimpleEvents extends JavaPlugin {
-	Logger log = Logger.getLogger("Minecraft");
+	public static Logger log = Logger.getLogger("Minecraft");
 	public void addListener(String substring) {
 		
 	}
